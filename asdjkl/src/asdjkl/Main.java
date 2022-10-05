076b9ca2acd0898431088bdcf8609d2d8cca5a41
Kruzikael014 <marchelhermanliansyah@gmail.com>
2022-10-05 14:44:33 +0700

    catatan apapun

---
+++ asdjkl/src/asdjkl/Main.java
@@ -0,0 +1,15 @@
+package asdjkl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nama = "Marchel";
+		
+
+		
+
+		
+	}
+	
+}
---
+++ asdjkl/src/asdjkl/ss.java
@@ -0,0 +1,15 @@
+package asdjkl;
+
+public class ss {
+	
+	private  int gaji = 55;
+
+	public int getGaji() {
+		return gaji;
+	}
+
+	public void setGaji(int gaji) {
+		this.gaji = gaji;
+	}
+
+}
